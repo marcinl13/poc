e80b26b31dd0729e553c008371446d1592b98370
marlepo <marcinlpotrykus@gmail.com>
2025-07-18 17:20:55 +0200

    feat(chat-app): @chat/shared

--- nextjs-clerk-chat/chat/src/app/chat-ws/[roomId]/ChatRoomWs.tsx
+++ nextjs-clerk-chat/chat/src/app/chat-ws/[roomId]/ChatRoomWs.tsx
@@ -4,7 +4,7 @@ import { ChatForm } from "@/components/ChatForm";
 import { ChatMembers } from "@/components/ChatMembers";
 import { ChatMessages } from "@/components/ChatMessages";
 import { useWsChat } from "@/hooks/useWsChat";
-import type { ChatRoomId, ChatUser } from "chat-app";
+import type { ChatRoomId, ChatUser } from "@chat/shared";
 import type { FC } from "react";
 
 export const ChatRoomWs: FC<{

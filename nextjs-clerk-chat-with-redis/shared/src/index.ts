af5935fecd59f2b51fd241799a5d5d4fbb8d6c8e
marlepo <marcinlpotrykus@gmail.com>
2025-07-23 08:21:11 +0200

    feat(chat-app-with-redis, shared): Initial commit

---
+++ nextjs-clerk-chat-with-redis/shared/src/index.ts
@@ -0,0 +1,20 @@
+export enum ChatRole {
+  Author = 'author',
+  Publisher = 'publisher',
+}
+
+// Type with only the values of Role
+export type ChatRoleValue = `${ChatRole}`;
+
+export type ChatRoomId = string;
+
+export type ChatUser = {
+  id: string;
+  email: string;
+  role: ChatRole;
+};
+
+export type ChatMessage = {
+  text: string;
+  user: ChatUser;
+};

12ea1055bbd847b5d53077c020261d41929e72b5
marlepo <marcinlpotrykus@gmail.com>
2025-07-25 16:54:55 +0200

    feat(chat-app-with-redis, client): load cached massages from redis

--- nextjs-clerk-chat-with-redis/chat/src/app/chat-ws/[roomId]/page.tsx
+++ nextjs-clerk-chat-with-redis/chat/src/app/chat-ws/[roomId]/page.tsx
@@ -1,17 +1,25 @@
+import Redis from "ioredis";
 import { getUserDataWithRole } from "@/data-access-layer/get-user-data-with-role";
 import { ChatRoomWs } from "./ChatRoomWs";
 
 type Params = Promise<{ roomId: string }>;
+const redis = new Redis("redis://:@localhost:6380/0");
 
 export default async function ChatRoomPage({ params }: { params: Params }) {
   const userDataWithRole = await getUserDataWithRole();
   const awaitParams = await params;
+  const redisKey = `chat:messages:${awaitParams.roomId}`;
+  const messages = await redis.lrange(redisKey, 0, -1);
 
   return (
     <main className="container px-4 max-w-3xl mx-auto mt-10 font-sans">
       <h1 className="text-3xl font-bold mb-6">Room: {awaitParams.roomId}</h1>
 
-      <ChatRoomWs roomId={awaitParams.roomId} userInfo={userDataWithRole} />
+      <ChatRoomWs
+        roomId={awaitParams.roomId}
+        userInfo={userDataWithRole}
+        cachedMessages={messages.map((m) => JSON.parse(m))}
+      />
     </main>
   );
 }

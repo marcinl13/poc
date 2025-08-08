9cfc54d1ae8404c31fb8a32270fddc31653a1d5e
marlepo <marcinlpotrykus@gmail.com>
2025-08-08 17:10:29 +0200

    feat: SignOut

--- better-auth/app/dashboard/page.tsx
+++ better-auth/app/dashboard/page.tsx
@@ -1,4 +1,11 @@
-import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { AuthSignOutButton } from "@/components/AuthSignOutButton";
+import {
+  Card,
+  CardAction,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 
@@ -8,12 +15,21 @@ export default async function Page() {
   });
 
   return (
-    <main>
-      <Card>
-        <CardHeader>
-          <CardTitle>Welcome {session?.user.name}</CardTitle>
-        </CardHeader>
-      </Card>
+    <main className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <div className="flex w-full max-w-sm flex-col gap-6">
+        <div className="flex flex-col gap-6">
+          <Card>
+            <CardHeader>
+              <CardTitle>Welcome {session?.user.name}</CardTitle>
+              <CardDescription>{session?.user.email}</CardDescription>
+
+              <CardAction>
+                <AuthSignOutButton />
+              </CardAction>
+            </CardHeader>
+          </Card>
+        </div>
+      </div>
     </main>
   );
 }

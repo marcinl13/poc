920a0248c5ecef2c212870b7f9fbcd682e154994
marlepo <marcinlpotrykus@gmail.com>
2025-08-08 17:12:15 +0200

    feat: socialProviders google

--- better-auth/lib/auth.ts
+++ better-auth/lib/auth.ts
@@ -6,6 +6,12 @@ export const auth = betterAuth({
   database: prismaAdapter(prisma, {
     provider: "postgresql",
   }),
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   emailAndPassword: {
     enabled: true,
   },
